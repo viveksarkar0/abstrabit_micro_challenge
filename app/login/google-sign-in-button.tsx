a82ce0466e71dffec129346c02e7efa8ce33ed90
vivek sarkar <vivek@viveks-MacBook-Air.local>
2026-02-16 23:16:55 +0530

    new cha'
    '
    
    '

--- app/login/google-sign-in-button.tsx
+++ app/login/google-sign-in-button.tsx
@@ -18,7 +18,7 @@ export default function GoogleSignInButton() {
 
             const supabase = createClient()
 
-            const origin = process.env.NEXT_PUBLIC_SITE_URL 
+            const origin = process.env.NEXT_PUBLIC_SITE_URL
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
